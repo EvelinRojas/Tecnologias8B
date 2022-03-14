a76220edee512d7c2b7aa0a4e2d3a6e739b2eca0
EvelinRojas <71529733+EvelinRojas@users.noreply.github.com>
2022-03-14 09:33:11 -0600

    Turorial de News

---
+++ web/app/Views/vistados.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr"
+    <head>
+        <meta charset="utf-8">
+        <title> SEgunda Vista</title>
+    </head>
+    <body>
+        <h1>estas en la vista dos</h1>
+    </body>
+</html>
